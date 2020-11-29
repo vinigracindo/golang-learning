8cf855a7d9b4fc5f8cc1d0fe2f30aab3b847c0e3
Vinnicyus Gracindo <vini.gracindo@gmail.com>
2020-11-28 22:49:23 -0300

    Pacotes em golang

---
+++ 1 - Pacotes/auxiliar/auxiliar.go
@@ -0,0 +1,11 @@
+package auxiliar
+
+import (
+	"fmt"
+)
+
+// Escrever mostra uma mensagem na tela
+func Escrever() {
+	fmt.Println("Pacote Auxiliar")
+	escrever2()
+}
---
+++ 1 - Pacotes/auxiliar/auxiliar2.go
@@ -0,0 +1,10 @@
+package auxiliar
+
+import (
+	"fmt"
+)
+
+// escrever 2 mostra mensagem na tela
+func escrever2() {
+	fmt.Println("Auxiliar 2")
+}
