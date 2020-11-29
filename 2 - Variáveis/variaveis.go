57d23313974fe5e3e5f27506afa38c40df8f3f89
Vinnicyus Gracindo <vini.gracindo@gmail.com>
2020-11-28 22:57:29 -0300

    Variáveis do tipo String

---
+++ 2 - Variáveis/variaveis.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+	var variavel1 string = "Variável do Tipo String" // Declaração explícita
+	variavel2 := "Variável 2"                        // Declaração implícita de variável
+	fmt.Println(variavel1)
+	fmt.Println(variavel2)
+
+	var (
+		variavel3 string = "Variável 3"
+		variavel4 string = "Variável 4"
+	)
+
+	fmt.Println(variavel3, variavel4)
+
+	variavel5, variavel6 := "Variável 5", "Variável 6"
+	fmt.Println(variavel5, variavel6)
+
+	const constante1 string = "Constante 1"
+	fmt.Println(constante1)
+
+}
