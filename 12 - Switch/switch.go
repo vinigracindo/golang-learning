0e140d46270af2b8f496b72a80e471338e119838
Vinnicyus Gracindo <vini.gracindo@gmail.com>
2021-01-09 21:14:47 -0300

    switch case

---
+++ 12 - Switch/switch.go
@@ -0,0 +1,52 @@
+package main
+
+import "fmt"
+
+func diaDaSemana(numero int) string {
+	switch numero {
+	case 1:
+		return "Domingo"
+	case 2:
+		return "Segunda"
+	case 3:
+		return "Terça"
+	case 4:
+		return "Quarta"
+	case 5:
+		return "Quinta"
+	case 6:
+		return "Sexta"
+	case 7:
+		return "Sábado"
+	default:
+		return "Número inválido"
+	}
+}
+
+func diaDaSemana2(numero int) string {
+	switch {
+	case numero == 1:
+		return "Domingo"
+	case numero == 2:
+		return "Segunda"
+	case numero == 3:
+		return "Terça"
+	case numero == 4:
+		return "Quarta"
+	case numero == 5:
+		return "Quinta"
+	case numero == 6:
+		return "Sexta"
+	case numero == 7:
+		return "Sábado"
+	default:
+		return "Número inválido"
+	}
+}
+
+func main() {
+	fmt.Println("Switch")
+
+	dia := diaDaSemana(30)
+	fmt.Println(dia)
+}
